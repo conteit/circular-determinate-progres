08ed9806bf1317254d3c1b0b283e9ee50c606e89
Paolo Contessi <paolo.contessi@videomed.biz>
2020-05-25 22:37:56 +0200

    Add license

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import { CircularDeterminateProgress } from 'circular-determinate-progress'
 import 'circular-determinate-progress/dist/index.css'
 
 const App = () => {
-  return <CircularDeterminateProgress percentage={0.9} strokeSize={10} rotating={true} />
+  const [ p, setP ] = useState({value: 0.0});
+  useEffect(() => {
+    let value = 0;
+    let inc = 0.01;
+    function update() {
+      if (value <= 0) {
+        inc = 0.01;
+      } else if (value >= 1) {
+        inc = -0.01;
+      }
+      value += inc;
+      setP({value});
+      setTimeout(update, 100)
+    }
+    update();
+  }, []);
+  return <CircularDeterminateProgress percentage={p.value} strokeSize={10} rotating={true} />
 }
 
 export default App
